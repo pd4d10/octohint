3b19bb05a16450ed632febf38a571ae546229921
pd4d10 <pd4d10@gmail.com>
2017-01-31 12:13:01 +0800

    Get code position of mouse click

---
+++ chrome/contentscript.js
@@ -0,0 +1,25 @@
+function getPosition(e, $dom) {
+  const rect = $dom.getBoundingClientRect()
+  return {
+    x: Math.floor((e.clientX - rect.left - 60) / 7.223),
+    y: Math.floor((e.clientY - rect.top) / 20)
+  }
+}
+
+function main() {
+  const $dom = document.querySelector('table')
+  if (!$dom) {
+    return
+  }
+
+  const code = $dom.innerText
+
+  $dom.addEventListener('click', function (e) {
+    const position = getPosition(e, $dom)
+    // console.log(e)
+    // console.log(e.offsetX, e.offsetY)
+    console.log(position)
+  })
+}
+
+main()
