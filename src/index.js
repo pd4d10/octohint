deae840b10f4ca27d9fb2867492ffc79249da92e
pd4d10 <pd4d10@gmail.com>
2017-02-01 15:28:07 +0800

    Add AST traverse

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import typescript from 'typescript'
+import ts from 'typescript'
+
+const w = 7.223
+const fileheadheight = 43
+const gutterwidth = 60
+const lineheight = 20
 
 function getPosition(e, $dom) {
   const rect = $dom.getBoundingClientRect()
+  // console.log(e.clientX, e.clientY, rect)
   return {
-    x: Math.floor((e.clientX - rect.left - 60) / 7.223),
-    y: Math.floor((e.clientY - rect.top) / 20)
+    x: Math.floor((e.clientX - rect.left - gutterwidth) / w),
+    y: Math.floor((e.clientY - rect.top) / lineheight)
   }
+}
+
+function visit(source, pos) {
+}
+
+function check(node, pos, cb) {
+  if (node.pos <= pos && pos < node.end) {
+    console.log(node)
+    if (node.kind === 70) {
+      return cb(node)
+    }
+
+    ts.forEachChild(node, n => check(n, pos, cb))
+  }
+}
+
+function checkPromise(node, pos) {
+  return new Promise(resolve => {
+    check(node, pos, resolve)
+  })
+}
+
+function draw(range, width) {
+  const $mask = document.createElement('div')
+  $mask.style = `position:absolute;width:${width * w}px;height:20px;top:${range.line * 20 + fileheadheight}px;left:${range.character * w + gutterwidth}px;background:rgba(0,0,255,0.3)`
+  const $container = document.querySelector('.blob-wrapper')
+  $container.appendChild($mask)
 }
 
 function main() {
@@ -15,12 +48,18 @@ function main() {
   }
 
   const code = $dom.innerText
+  const source = ts.createSourceFile('index.js', code)
 
-  $dom.addEventListener('click', function (e) {
+  $dom.addEventListener('click', async function (e) {
     const position = getPosition(e, $dom)
-    // console.log(e)
-    // console.log(e.offsetX, e.offsetY)
-    console.log(position)
+    const pos = source.getPositionOfLineAndCharacter(position.y, position.x)
+    // console.log(source)
+    const identifier = await checkPromise(source, pos)
+    // console.log(pos)
+    const range = source.getLineAndCharacterOfPosition(identifier.pos)
+    console.log(range)
+    const width = identifier.end - identifier.pos
+    draw(range, width)
   })
 }
 
