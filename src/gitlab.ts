6b473271967984ecb8788a87da0eefcc3f94a5ff
pd4d10 <pd4d10@gmail.com>
2017-04-11 00:01:43 +0800

    Fix GitLab missing empty line

--- src/gitlab.ts
+++ src/gitlab.ts
@@ -8,6 +8,11 @@ class GitLabRenderer extends Renderer {
     return <HTMLElement>document.querySelector('.blob-content code')
   }
 
+  getCode() {
+    const lines = this.$code.children
+    return [].map.call(lines, line => line.innerText).join('\n')
+  }
+
   getFontWidth() {
     const $ = <HTMLElement>document.querySelector('#LC1 > span')
     return $.getBoundingClientRect().width / $.innerText.length
