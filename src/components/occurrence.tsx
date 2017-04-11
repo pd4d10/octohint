36af9bfa506262b0dc934e0b4cb1a071862fc287
pd4d10 <pd4d10@gmail.com>
2017-04-12 02:50:48 +0800

    Fix colors to make it more highlighted

--- src/components/occurrence.tsx
+++ src/components/occurrence.tsx
@@ -12,7 +12,7 @@ const Occurrence = (props: OccurrenceProps) => (
   <div
     style={{
       position: 'absolute',
-      background: props.isWriteAccess ? 'rgba(14,99,156,.25)' : 'rgba(173,214,255,.3)',
+      background: props.isWriteAccess ? 'rgba(14,99,156,.4)' : 'rgba(173,214,255,.7)',
       width: props.width,
       height: props.height,
       top: props.top,
