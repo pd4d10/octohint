b02092a90453ceec068b036ed324283ed8628737
pd4d10 <pd4d10@gmail.com>
2017-04-17 00:12:29 +0800

    Use webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var StringReplacePlugin = require("string-replace-webpack-plugin");
 
 module.exports = {
   entry: {
+    // typescript: ['typescript'],
     background: './src/background',
     sentry: './src/sentry',
     github: './src/platforms/github',
@@ -60,5 +61,19 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin('chrome/dist'),
     new StringReplacePlugin()
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "typescript",
+
+    //   // filename: "vendor.js"
+    //   // (Give the chunk a different name)
+
+    //   minChunks: Infinity,
+    //   // (with more entries, this ensures that no other module
+    //   //  goes into the vendor chunk)
+    // })
+    // new webpack.optimize.AggressiveSplittingPlugin({
+    //   // minSize: 10000,
+    //   maxSize: 4 * 1024 * 1024,
+    // })
   ]
 }
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,17 +1,16 @@
 const webpack = require('webpack')
+const merge = require('webpack-merge')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const config = require('./webpack.config')
 
 // TODO Tree shaking
-
-config.devtool = false
-
-config.plugins = config.plugins.concat([
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production')
-  }),
-  new webpack.optimize.UglifyJsPlugin(),
-  new BundleAnalyzerPlugin()
-])
-
-module.exports = config
+module.exports = merge(config, {
+  devtool: false,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new BundleAnalyzerPlugin()
+  ]
+})
