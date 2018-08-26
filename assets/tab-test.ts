f543678ef33373c854a939bd066371b20db55383
Rongjian Zhang <pd4d10@gmail.com>
2018-08-26 23:12:18 +0800

    Add tab test file

---
+++ assets/tab-test.ts
@@ -0,0 +1,19 @@
+class Student {
+	fullName: string
+	constructor(public firstName, public middleInitial, public lastName) {
+		this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+	}
+}
+
+interface Person {
+	firstName: string
+	lastName: string
+}
+
+function greeter(person: Person) {
+	return 'Hello, ' + person.firstName + ' ' + person.lastName
+}
+
+let user = new Student('Jane', 'M.', 'User')
+
+document.body.innerHTML = greeter(user)
