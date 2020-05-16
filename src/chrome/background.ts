e070a77c96e1294f5ff275c89cad3a4ad7e137bc
Fregante <opensource@bfred.it>
2020-05-16 17:15:36 +0200

    Lint

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,4 +1,4 @@
-import 'webext-dynamic-content-scripts';
-import addDomainPermissionToggle from 'webext-domain-permission-toggle';
+import 'webext-dynamic-content-scripts'
+import addDomainPermissionToggle from 'webext-domain-permission-toggle'
 
-addDomainPermissionToggle();
+addDomainPermissionToggle()
