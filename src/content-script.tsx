ae170436e9df20cfb6b2177d3d3b35f4db651b4a
Rongjian Zhang <pd4d10@gmail.com>
2021-09-20 14:51:50 +0800

    refactor: listen document

--- src/content-script.tsx
+++ src/content-script.tsx
@@ -1,7 +1,9 @@
-import { render, h } from 'preact'
-import { App } from './app'
+import { render, h, Fragment } from 'preact'
+import { createPortal } from 'preact/compat'
 import githubInject from 'github-injection'
-import { slice } from 'lodash-es'
+import { slice, debounce } from 'lodash-es'
+import { HintRequest, HintResponse } from './types'
+import { JSXInternal } from 'preact/src/jsx'
 
 const toStyleText = (obj: { [key: string]: string | number }) => {
   return Object.entries(obj)
@@ -41,6 +43,20 @@ const px2num = (px: string | null) => {
   }
 }
 
+interface InitProps {
+  container: HTMLElement
+  $background: HTMLElement
+  $quickInfo: HTMLElement
+  fontWidth: number
+  fontFamily: string
+  fileName: string
+  code: string
+  lineHeight: number
+  tabSize: number
+}
+
+let initProps: InitProps | undefined
+
 /**
  * Principles:
  * Do not break DOM position as mush as possible
@@ -79,29 +95,32 @@ const px2num = (px: string | null) => {
  * <container> and its childrens should not set background-color
  * Order: background -> other childrens(including code) -> quickInfo
  */
-const renderToContainer = ({
-  container,
-  fontDom,
-  tabSizeDom,
-  lineHeight,
-  paddingLeft,
-  paddingTop,
-  code,
-  fileName,
-  beforeRender,
-}: RenderParams) => {
+const init = (e: MouseEvent, req: RenderRequest) => {
+  if (!(e.target instanceof HTMLElement)) return
+
+  if (initProps && document.contains(initProps.container)) return
+
+  // reset response
+  response = {}
+
+  const container = e.target.closest<HTMLElement>(req.selector)
+  if (!container) return
+
   console.log('container:', container)
 
-  const tabSize = parseInt(getComputedStyle(tabSizeDom).getPropertyValue('tab-size'), 10) || 8 // TODO: Firefox
+  const fontDom = $(req.fontSelector)
+  if (!fontDom) return
+
+  const tabSize = parseInt(getComputedStyle(fontDom).getPropertyValue('tab-size'), 10) || 8 // TODO: Firefox
 
   // Get font width and family
   const fontParams = getFontParams(fontDom)
 
   // TODO: This is pretty tricky for making GitLab and Bitbucket work
-  if (beforeRender) beforeRender()
+  // if (beforeRender) beforeRender()
 
   const containerRect = container.getBoundingClientRect()
-  const wrapperWidth = `${containerRect.width - paddingLeft - 10}px`
+  const wrapperWidth = `${containerRect.width - req.paddingLeft - 10}px`
 
   const $background = document.createElement('div')
   $background.setAttribute(
@@ -109,8 +128,8 @@ const renderToContainer = ({
     toStyleText({
       position: 'relative',
       // zIndex: -1, // Set z-index to -1 makes GitLab occurrence not show
-      top: paddingTop + 'px',
-      left: paddingLeft + 'px',
+      top: req.paddingTop + 'px',
+      left: req.paddingLeft + 'px',
       width: wrapperWidth,
     })
   )
@@ -128,30 +147,27 @@ const renderToContainer = ({
       position: 'relative',
       width: wrapperWidth, // Important, make quick info show as wide as possible
       // zIndex: 2,
-      bottom: `${containerRect.height - paddingAndBorderOfContainer - paddingTop}px`,
-      left: `${paddingLeft}px`,
+      bottom: `${containerRect.height - paddingAndBorderOfContainer - req.paddingTop}px`,
+      left: `${req.paddingLeft}px`,
     })
   )
 
   container.insertBefore($background, container.firstChild)
   container.appendChild($quickInfo)
 
-  render(
-    <App
-      {...{
-        container,
-        $background,
-        $quickInfo,
-        fontWidth: fontParams.width,
-        fontFamily: fontParams.family,
-        fileName,
-        code,
-        lineHeight,
-        tabSize,
-      }}
-    />,
-    $background
-  )
+  const lineHeight = fontDom.getBoundingClientRect().height
+
+  initProps = {
+    container,
+    $background,
+    $quickInfo,
+    fontWidth: fontParams.width,
+    fontFamily: fontParams.family,
+    fileName: req.getFileName(),
+    code: req.getCode(container),
+    lineHeight,
+    tabSize,
+  }
 }
 
 const $ = (selector: string, wrapper: HTMLElement = document.body) => {
@@ -161,70 +177,51 @@ const $$ = (selector: string, wrapper: HTMLElement = document.body) => {
   return slice(wrapper.querySelectorAll<HTMLElement>(selector))
 }
 
-// Replace `//` with `/` to simulate file system path
-function getFilePath(loc: { host: string; pathname: string } = location) {
-  return '/' + loc.host + loc.pathname
-}
-
-function getGithubCode(container: HTMLElement) {
-  return $$('tr>td:nth-child(2)', container).reduce((code, el) => {
-    return code + el.innerText.replaceAll('\n', '') + '\n'
-  }, '')
-}
-
-interface RenderParams {
-  container: HTMLElement
-  fontDom: HTMLElement
-  tabSizeDom: HTMLElement
-  lineWidth: number
-  lineHeight: number
+interface RenderRequest {
+  selector: string
+  fontSelector: string
   paddingLeft: number
   paddingTop: number
-  code: string
-  fileName: string
-  // TODO: This is pretty tricky for making GitLab and Bitbucket work
-  beforeRender?: () => void
+  getFileName(): string
+  getCode(container: HTMLElement): string
 }
 
-const getGithubParams = (): RenderParams | undefined => {
-  const container = $('.blob-wrapper')
-  const line1 = $('#LC1')
-  const tabSizeDom = $('.blob-wrapper table')
-  if (!container || !line1 || !tabSizeDom) return
-
-  const rect = line1.getBoundingClientRect()
-  return {
-    container,
-    fontDom: line1,
-    tabSizeDom,
-    lineWidth: rect.width,
-    lineHeight: rect.height,
-    paddingLeft: 60,
-    paddingTop: 0,
-    code: getGithubCode(container),
-    fileName: getFilePath(),
-  }
+const githubRenderRequest: RenderRequest = {
+  selector: '.blob-wrapper',
+  fontSelector: '#LC1',
+  paddingLeft: 60,
+  paddingTop: 0,
+  getFileName() {
+    // replace `//` with `/` to simulate file system path
+    return '/' + location.host + location.pathname
+  },
+  getCode(container) {
+    return $$('tr>td:nth-child(2)', container).reduce((code, el) => {
+      const line = el.innerText.replaceAll('\n', '') // empty line has an extra '\n', remove it
+      return code + line + '\n'
+    }, '')
+  },
 }
 
-function getGithubGistParams(wrapper: HTMLElement): RenderParams | undefined {
-  const container = $('.blob-wrapper', wrapper)
-  const fontDom = $('.blob-wrapper .blob-code', wrapper)
-  const tabSizeDom = $('.blob-wrapper table', wrapper)
-  const fileInfo = $('.file-info', wrapper)
-  if (!container || !fontDom || !tabSizeDom || !fileInfo) return
+// function getGithubGistParams(wrapper: HTMLElement): RenderParams | undefined {
+//   const container = $('.blob-wrapper', wrapper)
+//   const fontDom = $('.blob-wrapper .blob-code', wrapper)
+//   const tabSizeDom = $('.blob-wrapper table', wrapper)
+//   const fileInfo = $('.file-info', wrapper)
+//   if (!container || !fontDom || !tabSizeDom || !fileInfo) return
 
-  return {
-    container,
-    fontDom,
-    tabSizeDom,
-    lineWidth: 918,
-    lineHeight: 20,
-    paddingLeft: 60,
-    paddingTop: 0,
-    code: getGithubCode(container),
-    fileName: getFilePath().replace(/\/$/, '') + fileInfo.innerText.trim(),
-  }
-}
+//   return {
+//     container,
+//     fontDom,
+//     tabSizeDom,
+//     lineWidth: 918,
+//     lineHeight: 20,
+//     paddingLeft: 60,
+//     paddingTop: 0,
+//     code: getGithubCode(container),
+//     fileName: getFilePath().replace(/\/$/, '') + fileInfo.innerText.trim(),
+//   }
+// }
 
 // const BitbucketRenderer: RendererParams = {
 //   getContainer: () => $('.view-lines'),
@@ -287,48 +284,234 @@ function getGithubGistParams(wrapper: HTMLElement): RenderParams | undefined {
 //   })
 // }
 
-const runAdapter = () => {
-  // GitHub Gist
-  if (location.host === 'gist.github.com') {
-    const containers = $$('.js-gist-file-update-container')
-    if (!containers) return
-    containers.forEach((container) => {
-      const params = getGithubGistParams(container as HTMLElement)
-      if (params) {
-        renderToContainer(params)
-      }
+// GitHub Gist
+// if (location.host === 'gist.github.com') {
+//   $$('.js-gist-file-update-container')?.forEach((container) => {
+//     const params = getGithubGistParams(container as HTMLElement)
+//     if (params) {
+//       renderToContainer(params)
+//     }
+//   })
+// } else {
+//   // GitHub
+//   // TODO: Dynamic import
+//   // May be deployed at private domain, URL
+//   // So use DOM selector
+//   const params = getGithubParams()
+//   if (params) {
+//     githubInject(() => {
+//       const params = getGithubParams()
+//       if (params) renderToContainer(params)
+//     })
+//   } else {
+//   }
+// }
+
+const sendMessage = async (req: HintRequest) => {
+  console.log('req', req)
+
+  return new Promise<HintResponse>((resolve) => {
+    chrome.runtime.sendMessage(req, (response) => {
+      console.log('res', response)
+      resolve(response)
     })
-    return
-  }
-
-  // GitHub
-  // TODO: Dynamic import
-  // May be deployed at private domain, URL
-  // So use DOM selector
-  const params = getGithubParams()
-  if (params) {
-    githubInject(() => {
-      const params = getGithubParams()
-      if (params) renderToContainer(params)
-    })
-    return
-  }
-
-  // GitLab
-  // FIXME: Use `document.documentElement` may cause problems when DOM added by other extensions
-  // this.addMutationObserver(document.documentElement, GitLabRenderer, GitLabRenderer.getContainer() !== null)
-
-  // Direct loading, like browser go back
-  // renderToContainer(GitLabRenderer)
-  // return
-
-  // Bitbucket
-  // Seems Bitbucket already use monaco-editor to show code
-  // this.addMutationObserver($('.react-monaco-editor-container'), BitbucketRenderer)
-  // if (BitbucketRenderer.getContainer()) {
-  //   new Renderer(BitbucketRenderer)
-  //   return
-  // }
+  })
 }
 
-runAdapter()
+const getPosition = (e: MouseEvent, lineHeight: number, fontWidth: number, $background: HTMLElement) => {
+  const rect = $background.getBoundingClientRect()
+
+  // must be integers
+  const line = Math.floor((e.clientY - rect.top) / lineHeight)
+  const character = Math.floor((e.clientX - rect.left) / fontWidth)
+
+  if (line > 0 && character > 0) {
+    return { line, character }
+  }
+}
+
+const isMacOS = /Mac OS X/i.test(navigator.userAgent)
+
+const colors = {
+  quickInfoBg: 'rgba(173,214,255,.3)',
+  occurrenceWrite: 'rgba(14,99,156,.4)',
+  occurrenceRead: 'rgba(173,214,255,.7)',
+}
+
+function getColorFromKind(kind: string) {
+  switch (kind) {
+    case 'keyword':
+      return '#00f'
+    case 'punctuation':
+      return '#000'
+    default:
+      return '#001080'
+  }
+}
+
+let response: HintResponse = {}
+
+const handleResponse = (res: HintResponse, props: InitProps) => {
+  response = { ...response, ...res }
+  const { definition, occurrences, quickInfo } = response
+
+  if (definition) {
+    const line = definition.line + 1
+    document.querySelector<HTMLElement>('#L' + line)?.click() // TODO: reset definition
+  }
+
+  render(
+    <>
+      {occurrences?.map((occurrence) => (
+        <div
+          style={{
+            position: 'absolute',
+            background: occurrence.isWriteAccess ? colors.occurrenceWrite : colors.occurrenceRead,
+            width: occurrence.width * props.fontWidth,
+            height: props.lineHeight,
+            top: occurrence.range.line * props.lineHeight,
+            left: occurrence.range.character * props.fontWidth,
+          }}
+        />
+      ))}
+      {quickInfo && (
+        <div
+          style={{
+            position: 'absolute',
+            background: colors.quickInfoBg,
+            // lineHeight: '20px',
+            top: quickInfo.range.line * props.lineHeight,
+            left: quickInfo.range.character * props.fontWidth,
+            width: quickInfo.width * props.fontWidth,
+            height: props.lineHeight,
+          }}
+        />
+      )}
+      {quickInfo?.info &&
+        createPortal(
+          <div
+            style={{
+              whiteSpace: 'pre-wrap',
+              position: 'absolute',
+              backgroundColor: '#efeff2',
+              border: `1px solid #c8c8c8`,
+              fontSize: 12,
+              padding: `2px 4px`,
+              fontFamily: props.fontFamily,
+              left: quickInfo.range.character * props.fontWidth,
+              maxWidth: 500,
+              maxHeight: 300,
+              overflow: 'auto',
+              wordBreak: 'break-all',
+              ...(() => {
+                // TODO: Fix https://github.com/Microsoft/TypeScript/blob/master/Gulpfile.ts
+                // TODO: Show info according to height
+                // TODO: Make quick info could be copied
+                // For line 0 and 1, show info below, this is tricky
+                // To support horizontal scroll, our root DOM must be inside $('.blob-wrapper')
+                // So quick info can't show outside $('.blob-wrapper')
+                const positionStyle: JSXInternal.HTMLAttributes['style'] = {}
+                if (quickInfo.range.line < 2) {
+                  positionStyle.top = (quickInfo.range.line + 1) * props.lineHeight
+                } else {
+                  positionStyle.bottom = 0 - quickInfo.range.line * props.lineHeight
+                }
+
+                return positionStyle
+              })(),
+            }}
+          >
+            {
+              typeof quickInfo.info === 'string'
+                ? quickInfo.info.replace(/\\/g, '')
+                : quickInfo.info.map((part) => {
+                    if (part.text === '\n') {
+                      return <br />
+                    }
+                    return <span style={{ color: getColorFromKind(part.kind) }}>{part.text}</span>
+                  })
+
+              // JSON.parse(`"${info}"`)
+            }
+          </div>,
+          props.$quickInfo
+        )}
+    </>,
+    props.$background
+  )
+}
+
+// click: show occurrences
+// if meta key is pressed, also show definition and scroll to it
+document.addEventListener('click', async (e) => {
+  init(e, githubRenderRequest)
+  if (!initProps) return
+
+  console.log('click', e)
+
+  const position = getPosition(e, initProps.lineHeight, initProps.fontWidth, initProps.$background)
+  if (!position) return
+
+  const res = await sendMessage({
+    type: 'click',
+    file: initProps.fileName,
+    meta: isMacOS ? e.metaKey : e.ctrlKey,
+    code: initProps.code,
+    tabSize: initProps.tabSize,
+    ...position,
+  })
+  handleResponse(res, initProps)
+
+  // TODO: Exclude click event triggered by selecting text
+  // https://stackoverflow.com/questions/10390010/jquery-click-is-triggering-when-selecting-highlighting-text
+  // if (window.getSelection().toString()) {
+  //   return
+  // }
+})
+
+// mousemove: show quick info on stop
+document.addEventListener(
+  'mousemove',
+  debounce(async (e: MouseEvent) => {
+    init(e, githubRenderRequest)
+    if (!initProps) return
+
+    // console.log('mousemove', e)
+    const position = getPosition(e, initProps.lineHeight, initProps.fontWidth, initProps.$background)
+    if (!position) return
+
+    const res = await sendMessage({
+      type: 'hover',
+      file: initProps.fileName,
+      code: initProps.code,
+      tabSize: initProps.tabSize,
+      ...position,
+    })
+    handleResponse(res, initProps)
+  }, 300)
+)
+
+// mouseout: hide quick info on leave
+document.addEventListener('mouseout', (e) => {
+  init(e, githubRenderRequest)
+  if (!initProps) return
+
+  // console.log('mouseout', e)
+  handleResponse({ quickInfo: undefined }, initProps)
+})
+
+// GitLab
+// FIXME: Use `document.documentElement` may cause problems when DOM added by other extensions
+// this.addMutationObserver(document.documentElement, GitLabRenderer, GitLabRenderer.getContainer() !== null)
+
+// Direct loading, like browser go back
+// renderToContainer(GitLabRenderer)
+// return
+
+// Bitbucket
+// Seems Bitbucket already use monaco-editor to show code
+// this.addMutationObserver($('.react-monaco-editor-container'), BitbucketRenderer)
+// if (BitbucketRenderer.getContainer()) {
+//   new Renderer(BitbucketRenderer)
+//   return
+// }
